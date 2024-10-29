817ed8816d452d3d5f3089dd1477ef46252c21c7
강수연 <kangtea9@naver.com>
2024-10-29 12:13:42 +0900

    feat: 버튼 공통 컴포넌트 생성

---
+++ client/src/components/button/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './Button.module.css';
+
+type ButtonStyleProps = {
+  size: 'small' | 'medium' | 'large'; 
+  colorType: 'green' | 'red' | 'blue' | 'navy' | 'purple'; 
+};
+
+interface ButtonProps extends ButtonStyleProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick, size, colorType }) => {
+  const buttonClass = classNames(
+    styles.button,
+    styles[size],
+    styles[`${colorType}Normal`] // 예: greenNormal
+  );
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
