26e85077c89574d08cb5258a6cdb6e2252dfae2f
조성인 <sijo0209@naver.com>
2024-10-29 12:18:17 +0900

    feat: 모달 공통 컴포넌트 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Modal from "./components/CommonModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <>
@@ -18,8 +22,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={openModal} style={{ padding: '10px 20px', backgroundColor: '#007BFF', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+          Open Modal
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -28,6 +32,37 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        // title="유저 삭제"
+        title={
+          <>
+            제목도 두줄 가능합니까<br />
+            일단은요
+          </>
+        }
+        content={
+          // "정말로 삭제하시겠습니까?"
+          <>
+            정말로 삭제하시겠습니까?<br />
+            삭제하면 되돌릴 수 없습니다.
+          </>
+        }
+        footer={
+          <>
+            <button style={{ padding: '10px 20px', backgroundColor: 'blue', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+              파랑
+            </button>
+            <button style={{ padding: '10px 20px', backgroundColor: 'red', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+              빨강
+            </button>
+          </>
+        }
+        // width={400}
+        height={250}
+      />
     </>
   )
 }
