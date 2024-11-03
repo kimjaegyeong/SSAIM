31ce431fa76609b69b3e3f3b6e172455c54670ef
김재경 <dongji_11@naver.com>
2024-11-03 23:04:45 +0900

    feat : Project 조회에 필요한 request, response DTO 정의

---
+++ server/src/main/java/com/e203/project/dto/response/ProjectFindResponseDto.java
@@ -0,0 +1,39 @@
+package com.e203.project.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ProjectFindResponseDto {
+	private int id;
+	private String name;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private String jiraApi;
+	private String gitlabApi;
+	private Double progress_front;
+	private Double progress_back;
+	private ArrayList<ProjectMemberFindResponseDto> projectMemberFindResponseDtoList;
+
+	@Builder
+	private ProjectFindResponseDto(int id, String name, LocalDateTime startDate, LocalDateTime endDate,
+		String jiraApi, String gitlabApi,
+		ArrayList<ProjectMemberFindResponseDto> dto, Double progress_front, Double progress_back) {
+		this.id = id;
+		this.projectMemberFindResponseDtoList = dto;
+		this.name = name;
+		this.jiraApi = jiraApi;
+		this.gitlabApi = gitlabApi;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.progress_front = progress_front;
+		this.progress_back = progress_back;
+	}
+}
+
+
