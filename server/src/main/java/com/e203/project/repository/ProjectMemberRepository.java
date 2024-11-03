727112db29384f7d698dc38c8ef17765d6e288ad
김재경 <dongji_11@naver.com>
2024-11-03 23:06:18 +0900

    feat : Project 조회 기능 구현

--- server/src/main/java/com/e203/project/repository/ProjectMemberRepository.java
+++ server/src/main/java/com/e203/project/repository/ProjectMemberRepository.java
@@ -2,11 +2,17 @@ package com.e203.project.repository;
 
 import com.e203.project.entity.ProjectMember;
 import com.e203.user.entity.User;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Integer> {
 
-    ProjectMember findByUser(User user);
+	ProjectMember findByUser(User user);
+
+    @EntityGraph(attributePaths = {"project", "user"})
+    List<ProjectMember> findByProjectId(Integer projectId);
+
 }
--- server/src/main/java/com/e203/project/service/ProjectService.java
+++ server/src/main/java/com/e203/project/service/ProjectService.java
@@ -1,6 +1,9 @@
 package com.e203.project.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,11 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 import com.e203.project.dto.request.ProjectMemberCreateRequestDto;
 import com.e203.project.dto.request.ProjectCreateRequestDto;
 
+import com.e203.project.dto.response.ProjectFindResponseDto;
+import com.e203.project.dto.response.ProjectMemberFindResponseDto;
 import com.e203.project.entity.Project;
 import com.e203.project.entity.ProjectMember;
-import com.e203.project.repository.ProjectMemberRepository;
 import com.e203.project.repository.ProjectRepository;
 import com.e203.user.entity.User;
+import com.e203.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,7 +25,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProjectService {
 	private final ProjectRepository projectRepository;
-	private final ProjectMemberRepository projectMemberRepository;
+	private final ProjectMemberService projectMemberService;
+	private final UserRepository userRepository;
+
 	@Transactional
 	public boolean createProject(ProjectCreateRequestDto projectCreateRequestDto) {
 
@@ -30,14 +37,57 @@ public class ProjectService {
 		//ProjectMember 생성
 		for(int i=0; i<projectCreateRequestDto.getTeamMembers().size(); i++){
 			ProjectMember projectMember = createProjectMember(project, projectCreateRequestDto.getTeamMembers().get(i));
-			projectMemberRepository.save(projectMember);
+			projectMemberService.save(projectMember);
 		}
 		//if project, projectMember가 잘 저장되었다면
 		return true;
 	}
 
+	public Project findEntity(Integer projectId) {
+		Optional<Project> project = projectRepository.findById(projectId);
+		return project.orElse(null);
+	}
+
+	public ProjectFindResponseDto findProjectInfo(Integer projectId) {
+		Project project = findEntity(projectId);
+		List<ProjectMemberFindResponseDto> projectMemberFindResponseDtoList;
+		List<ProjectMember> projectMembers = projectMemberService.findEntityList(projectId);
+
+		//projectMembers를 dto로 바꾸기
+		projectMemberFindResponseDtoList = createProjectMemberFindResponseDtos(projectMembers);
+
+		//projectMember + project
+		ProjectFindResponseDto projectFindResponseDto = ProjectFindResponseDto.builder()
+			.id(project.getId())
+			.name(project.getName())
+			.startDate(project.getStartDate())
+			.endDate(project.getEndDate())
+			.jiraApi(project.getJiraApi())               // 프로젝트의 Jira API 정보
+			.gitlabApi(project.getGitlabApi())           // 프로젝트의 GitLab API 정보
+			.progress_back(project.getProgressBack())    // 백엔드 진행도
+			.progress_front(project.getProgressFront())  // 프론트엔드 진행도
+			.build();
+
+		return projectFindResponseDto;
+	}
+
+	private List<ProjectMemberFindResponseDto> createProjectMemberFindResponseDtos(List<ProjectMember> projectMembers) {
+		List<ProjectMemberFindResponseDto> projectMemberFindResponseDtoList = new ArrayList<>();
+
+		for (ProjectMember projectMember : projectMembers) {
+			ProjectMemberFindResponseDto dto = ProjectMemberFindResponseDto.builder()
+				.id(projectMember.getId())
+				.name(projectMember.getUser().getUserName())
+				.role(projectMember.getRole())
+				.profileImage(projectMember.getUser().getUserProfileImage())
+				.build();
+			projectMemberFindResponseDtoList.add(dto);
+		}
+		return projectMemberFindResponseDtoList;
+	}
+
 	private ProjectMember createProjectMember(Project project, ProjectMemberCreateRequestDto member) {
-		User user = userTestStub(member);
+ 		User user = userRepository.findById(member.getId()).orElse(null);
 		return ProjectMember.builder()
 				.user(user)
 				.project(project)
@@ -45,23 +95,6 @@ public class ProjectService {
 				.build();
 	}
 
-	private User userTestStub(ProjectMemberCreateRequestDto member){
-		User user = User.builder().userEmail("test@test.com")
-			.userName("name")
-			.userClass(1)
-			.userCampus(2)
-			.userGeneration(11)
-			.userPw("34234234234")
-			.userNickname("testset")
-			.userBirth(LocalDate.now())
-			.userGender(2)
-			.userPhone("010-1111-1111")
-			.userProfileImage("https:s3//image")
-			.userSkills("spring boot")
-			.userProfileMessage("안녕하세요")
-			.build();
-		user.setUserId(member.getId());
-		return user;
-	}
+
 }
 
