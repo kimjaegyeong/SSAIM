c6c2657dc4e4e220b10d77b4c4b91ef64499f2a6
여대기 <daegi0923@naver.com>
2024-11-03 20:27:33 +0900

    design : project create page 구현

--- client/src/features/project/components/projectCreate/ProjectCreate.tsx
+++ client/src/features/project/components/projectCreate/ProjectCreate.tsx
@@ -1,19 +1,71 @@
-import styles from './ProjectCreate.module.css'
+import styles from './ProjectCreate.module.css';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import Button from '../../../../components/button/Button';
 
+const ProjectCreate = () => {
+  const [startDate, setStartDate] = useState<Date|null>(null);
+  const [endDate, setEndDate] = useState<Date|null>(null);
+  const [image] = useState(null);
 
+  const handleImageClick = () => {
+    // 이미지 선택 로직
+  };
 
+  return (
+    <div className={styles.container}>
+      {/* 상단 구역 */}
+      <div className={styles.topSection}>
+        {/* 왼쪽 구역 - 대표 사진 */}
+        <div className={styles.imageSection} onClick={handleImageClick}>
+          {image ? (
+            <img src={image} alt="대표 사진" className={styles.image} />
+          ) : (
+            <div className={styles.imagePlaceholder}>사진 추가</div>
+          )}
+        </div>
+        {/* 오른쪽 구역 - 프로젝트 이름 및 팀 이름 */}
+        <div className={styles.infoSection}>
+          <input type="text" placeholder="프로젝트 이름" className={styles.input} />
+          <input type="text" placeholder="팀 이름" className={styles.input} />
+        </div>
+      </div>
 
-const projectCreate = () => {
+      {/* 팀원 추가 모달 (현재 비워둠) */}
+      <div className={styles.teamMemberModal}>팀원 추가 모달 자리</div>
 
+      {/* 프로젝트 기간 입력 */}
+      <div className={styles.dateSection}>
+        <div>
+          <label>프로젝트 시작일자:</label>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="yyyy/MM/dd"
+            placeholderText="시작일 선택"
+            className={styles.dateInput}
+          />
+        </div>
+        <span className={styles.dateSeparator}>~</span>
+        <div>
+          <label>프로젝트 종료일자:</label>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            dateFormat="yyyy/MM/dd"
+            placeholderText="종료일 선택"
+            className={styles.dateInput}
+          />
+        </div>
+      </div>
+      <hr />
+      <div className={styles.footer}>
+          <Button children="프로젝트 생성" colorType='blue' size='small'></Button>
 
-return <div className={styles.container}>
-  <div>
-    
-  </div>
-  프로젝트 생성!
-</div>
-}
+      </div>
+    </div>
+  );
+};
 
-
-
-export default projectCreate
+export default ProjectCreate;
