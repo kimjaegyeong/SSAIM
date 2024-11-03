204b1c9c14eb6ddbdb8de2dc18e4150b36328753
강수연 <kangtea9@naver.com>
2024-11-04 00:42:13 +0900

    design: 우개이 메인 페이지 디자인 완료

--- client/src/pages/remind/RemindCreatePage.tsx
+++ client/src/pages/remind/RemindCreatePage.tsx
@@ -1,15 +1,15 @@
-import styles from './RemindListPage.module.css'
+import styles from './RemindCreatePage.module.css'
 import remindBG from '../../assets/remind/remindBG.png'
 
 
-const RemindListPage = () => {
+const RemindCreatePage = () => {
   return (
   <div>
     <img src={remindBG} alt="remindBG" className={styles.remindBG}/>
     <div className={styles.container}>
       <div className={styles.remindIntroText}>
         <div className={styles.remindIntroDesc}>
-            프로젝트를 마무리하며<br /> 나의 개발 이야기를 생성해보세요
+            우리가 함께 만드는 개발 이야기를 생성할<br /> 프로젝트를 선택해주세요
         </div>
       </div>
       <div className={styles.remindButton}>
@@ -22,4 +22,4 @@ const RemindListPage = () => {
   );
 };
 
-export default RemindListPage;
+export default RemindCreatePage;
