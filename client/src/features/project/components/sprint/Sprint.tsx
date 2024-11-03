7dba371efa7a4549995df7f5a1aa94e83462364c
여대기 <daegi0923@naver.com>
2024-11-04 02:01:11 +0900

    design : 스프린트 생성 모달

--- client/src/features/project/components/sprint/Sprint.tsx
+++ client/src/features/project/components/sprint/Sprint.tsx
@@ -1,26 +1,56 @@
 import styles from './Sprint.module.css';
 import Issue from './Issue';
+import Button from '../../../../components/button/Button';
+import { useState } from 'react';
+import SprintCreateModal from './SprintCreateModal';
 
 const WeeklyProgress = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleModalOpen =()=> {
+    setIsModalOpen(true);
+  }
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  }
   return (
-    <div className={styles.weeklyProgressContainer}>
-      {/* 요일 헤더 */}
+    <>
       <div className={styles.header}>
-        {['월', '화', '수', '목', '금'].map((day) => (
-          <div key={day} className={styles.dayHeader}>{day}</div>
-        ))}
+        <div className={styles.teamProfiles}>
+          <div className={styles.profilePicture}></div>
+          <div className={styles.profilePicture}></div>
+          <div className={styles.profilePicture}></div>
+        </div>
+        <h2 className={styles.sprintTitle}>스프린트 제목</h2>
+        <div className={styles.buttonPlaceholder}>
+          <Button children="스프린트 생성" colorType="blue" size="small" onClick={handleModalOpen}></Button>
+        </div>
       </div>
+      <div className={styles.weeklyProgressContainer}>
+        {/* 요일 헤더 */}
+        <div className={styles.contentheader}>
+          {['월', '화', '수', '목', '금'].map((day) => (
+            <div key={day} className={styles.dayHeader}>
+              {day}
+            </div>
+          ))}
+        </div>
 
-      {/* 요일별 이슈 */}
-      <div className={styles.issueGrid}>
-        {['월', '화', '수', '목', '금'].map((day) => (
-          <div key={day} className={styles.dayColumn}>
-            <Issue />
-            <Issue />
-          </div>
-        ))}
+        {/* 요일별 이슈 */}
+        <div className={styles.issueGrid}>
+          {['월', '화', '수', '목', '금'].map((day) => (
+            <div key={day} className={styles.dayColumn}>
+              <Issue />
+              <Issue />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      {
+        isModalOpen && (
+          <SprintCreateModal onClose={handleModalClose}/>
+        )
+      }
+    </>
   );
 };
 
