92566c0805c73501f44604d09cf2fe657511ba30
김재경 <dongji_11@naver.com>
2024-11-04 12:43:18 +0900

    feat : 사용자로부터 jira api key 를 받아서 db에 저장하는 기능 구현

---
+++ server/src/main/java/com/e203/project/service/JiraService.java
@@ -0,0 +1,27 @@
+package com.e203.project.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.e203.project.dto.request.ProjectJiraConnectDto;
+import com.e203.project.entity.Project;
+import com.e203.project.repository.ProjectMemberRepository;
+import com.e203.project.repository.ProjectRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JiraService {
+	// jira api key를 db에 저장하기
+	private final ProjectRepository projectRepository;
+	@Transactional
+	public String setJiraApi(ProjectJiraConnectDto jiraApi, int projectId){
+		Project project = projectRepository.findById(projectId).orElse(null);
+		if(project==null){
+			return null;
+		}
+		project.setJiraApi(jiraApi.getJiraApi());
+		return project.getJiraApi() ;
+	}
+}
