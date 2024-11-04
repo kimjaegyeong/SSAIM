800714858b8fc880246b4dc1fe0fd4a08a379312
여대기 <daegi0923@naver.com>
2024-11-04 14:59:44 +0900

    chore : build 스크립트 수정 및 절대경로 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-    ]
-  }
-})
+  plugins: [react(), tsconfigPaths()],
+});
