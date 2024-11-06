5bed72793f0bee385020d4cb547c239097d8007e
김재경 <dongji_11@naver.com>
2024-11-06 23:26:52 +0900

    feat : jira 전체 이슈 조회 기능 구현

--- server/src/main/java/com/e203/project/controller/JiraController.java
+++ server/src/main/java/com/e203/project/controller/JiraController.java
@@ -2,12 +2,17 @@ package com.e203.project.controller;
 
 import static org.springframework.http.HttpStatus.*;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.e203.project.dto.request.JiraIssueResponseDto;
 import com.e203.project.dto.request.ProjectJiraConnectDto;
 import com.e203.project.service.JiraService;
 
@@ -20,14 +25,28 @@ public class JiraController {
 	private final JiraService jiraService;
 
 	@PostMapping("/api/v1/projects/{projectId}/jira-api")
-	public ResponseEntity<String> connectJiraApi(@PathVariable("projectId") int projectId , @RequestBody
-		ProjectJiraConnectDto projectJiraConnectDto) {
+	public ResponseEntity<String> connectJiraApi(@PathVariable("projectId") int projectId
+		, @RequestBody ProjectJiraConnectDto projectJiraConnectDto) {
 
 		boolean result = jiraService.setJiraApi(projectJiraConnectDto, projectId);
 
-		if(result){
+		if (result) {
 			return ResponseEntity.status(OK).body("jira api 연결 성공");
 		}
 		return ResponseEntity.status(BAD_REQUEST).body("jira api 연결 실패");
 	}
+
+	@GetMapping("/api/v1/projects/{projectId}/issue")
+	public ResponseEntity<List<JiraIssueResponseDto>> findAllJiraIssue(@PathVariable("projectId") int projectId,
+		@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) {
+		List<JiraIssueResponseDto> allJiraIssues = jiraService.findAllJiraIssues(startDate, endDate, projectId);
+		if(allJiraIssues==null){
+			return ResponseEntity.status(NOT_FOUND).body(null);
+		}
+		if(allJiraIssues.isEmpty()){
+			return ResponseEntity.status(NOT_FOUND).body(null);
+		}
+		return ResponseEntity.status(OK).body(allJiraIssues);
+	}
+
 }
--- server/src/main/java/com/e203/project/service/JiraService.java
+++ server/src/main/java/com/e203/project/service/JiraService.java
@@ -1,12 +1,21 @@
 package com.e203.project.service;
 
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClient;
 
+import com.e203.project.dto.request.JiraIssueResponseDto;
 import com.e203.project.dto.request.ProjectJiraConnectDto;
+import com.e203.project.dto.jiraapi.JiraResponse;
 import com.e203.project.entity.Project;
+import com.e203.project.entity.ProjectMember;
 import com.e203.project.repository.ProjectMemberRepository;
 import com.e203.project.repository.ProjectRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,13 +24,63 @@ import lombok.RequiredArgsConstructor;
 public class JiraService {
 	// jira api key를 db에 저장하기
 	private final ProjectRepository projectRepository;
+	private final ProjectMemberRepository projectMemberRepository;
+	private final RestClient restClient;
+	private final ObjectMapper objectMapper;
+	private final String JIRA_URL = "https://ssafy.atlassian.net/rest/api/3/search";
+
 	@Transactional
-	public Boolean setJiraApi(ProjectJiraConnectDto jiraApi, int projectId){
+	public Boolean setJiraApi(ProjectJiraConnectDto jiraApi, int projectId) {
 		Project project = projectRepository.findById(projectId).orElse(null);
-		if(project==null){
+		if (project == null) {
 			return false;
 		}
+		System.out.println(jiraApi.getJiraApi());
 		project.setJiraApi(jiraApi.getJiraApi());
-		return true ;
+		return true;
+	}
+
+	@Transactional
+	public List<JiraIssueResponseDto> findAllJiraIssues(String startDate, String endDate, int projectId) {
+		JiraResponse jiraIssues = getJiraIssues(startDate, endDate, projectId);
+		return jiraIssues.getIssues() == null ? null : jiraIssues.getIssues().stream()
+			.map(JiraIssueResponseDto::transferDto)
+			.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public JiraResponse getJiraIssues(String startDate, String endDate, int projectId) {
+		ProjectMember leader = getProjectLeader(projectId);
+		String jiraApi = leader.getProject().getJiraApi();
+		String userEmail = leader.getUser().getUserEmail();
+
+		String jql = "project=S11P31E203 AND created >= \"" + startDate + "\" AND created <= \"" + endDate + "\"";
+		String fields = "summary,status,assignee,customfield_10014,customfield_10031";
+		String url = JIRA_URL + "?jql=" + jql + "&fields=" + fields;
+
+		String encodedCredentials = Base64.getEncoder().encodeToString((userEmail + ":" + jiraApi).getBytes());
+
+		try {
+			String responseBody = restClient.get()
+				.uri(url)
+				.header("Authorization", "Basic " + encodedCredentials)
+				.header("Content-Type", "application/json")
+				.retrieve()
+				.body(String.class);
+			return objectMapper.readValue(responseBody, JiraResponse.class); // JSON 응답을 JiraResponse 객체로 변환
+		} catch (Exception e) {
+			// 예외 처리
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private ProjectMember getProjectLeader(int projectId) {
+		List<ProjectMember> leader = projectMemberRepository.findByProjectIdAndRole(projectId, 1);
+		if (leader.size() != 1) {
+			return null;
+		}
+		return leader.get(0);
+
 	}
 }
