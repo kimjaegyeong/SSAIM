ebbbe71b777910549e99420506393acf8f25ad13
김재경 <dongji_11@naver.com>
2024-11-06 23:27:18 +0900

    feat : jira 전체 이슈 조회 response dto 정의

---
+++ server/src/main/java/com/e203/project/dto/request/JiraIssueResponseDto.java
@@ -0,0 +1,38 @@
+package com.e203.project.dto.request;
+
+import com.e203.project.dto.jiraapi.JiraIssue;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class JiraIssueResponseDto {
+	private String title;
+	private String epicCode;
+	private String progress;
+	private double storyPoint;
+	private String allocator;
+
+	@Builder
+	private JiraIssueResponseDto(String title, String epicCode, String progress, double storyPoint, String allocator) {
+		this.title = title;
+		this.epicCode = epicCode;
+		this.progress = progress;
+		this.storyPoint = storyPoint;
+		this.allocator = allocator;
+	}
+
+	public static JiraIssueResponseDto transferDto(JiraIssue jiraIssue){
+		return JiraIssueResponseDto.builder()
+			.title(jiraIssue.getFields().getSummary())
+			.epicCode(jiraIssue.getFields().getEpicCode())
+			.progress(jiraIssue.getFields().getStatus().getProgress())
+			.storyPoint(jiraIssue.getFields().getStoryPoint() ==null ? 0 : jiraIssue.getFields().getStoryPoint())
+			.allocator(jiraIssue.getFields().getAssignee() == null ? "할당되지 않음" : jiraIssue.getFields().getAssignee().getDisplayName())
+			.build();
+	}
+}
