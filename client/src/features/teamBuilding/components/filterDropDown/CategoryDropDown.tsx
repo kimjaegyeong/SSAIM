06a9bafec87a7b2457138c758e7268d486c3cd00
조성인 <sijo0209@naver.com>
2024-11-07 11:38:40 +0900

    !HOTFIX: 빌드 에러 수정

--- client/src/features/teamBuilding/components/filterDropDown/CategoryDropDown.tsx
+++ client/src/features/teamBuilding/components/filterDropDown/CategoryDropDown.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from './CategoryDropdown.module.css';
+import styles from './CategoryDropDown.module.css';
 
 interface Option {
   value: string;
