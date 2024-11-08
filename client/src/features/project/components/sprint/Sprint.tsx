b98f2c5f6915cbcc25aede9b0227badbab4cd995
여대기 <daegi0923@naver.com>
2024-11-08 12:36:57 +0900

    feat : 주간 스프린트 페이지 데이터 로드

--- client/src/features/project/components/sprint/Sprint.tsx
+++ client/src/features/project/components/sprint/Sprint.tsx
@@ -3,15 +3,51 @@ import Issue from './Issue';
 import Button from '../../../../components/button/Button';
 import { useState } from 'react';
 import SprintCreateModal from './SprintCreateModal';
-
+import { useSprintIssueQuery } from '../../hooks/useSprintIssueData';
+import { useProjectInfo } from '../../hooks/useProjectInfo';
+import { useParams } from 'react-router-dom';
+import calculateWeeks from '../../utils/calculateWeeks';
+import { dateToString } from '@/utils/dateToString';
 const WeeklyProgress = () => {
+  const { projectId } = useParams<{ projectId: string }>();
+
+  const { data: projectInfo } = useProjectInfo(Number(projectId));
+  // const projectWeekList = calculateWeeks(projectInfo.startDate, projectInfo.endDate);
+  const projectWeekList = calculateWeeks(
+    new Date(projectInfo.startDate as Date),
+    new Date(projectInfo.endDate as Date)
+  );
+  const [currentWeek, setCurrentWeek] = useState(projectWeekList.length);
+  const { data: sprintIssues } = useSprintIssueQuery(
+    Number(projectId),
+    dateToString(projectWeekList[currentWeek - 1]?.startDate, '-'),
+    dateToString(projectWeekList[currentWeek - 1]?.endDate, '-')
+  );
+  console.log(projectWeekList);
+  console.log(
+    sprintIssues,
+    dateToString(projectWeekList[currentWeek - 1]?.startDate, '-'),
+    dateToString(projectWeekList[currentWeek - 1]?.endDate, '-')
+  );
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleModalOpen =()=> {
+  const handleModalOpen = () => {
     setIsModalOpen(true);
-  }
+  };
   const handleModalClose = () => {
     setIsModalOpen(false);
-  }
+  };
+
+  const handleIncreaseWeek = () => {
+    if (currentWeek < projectWeekList.length - 1) {
+      setCurrentWeek(currentWeek + 1);
+    }
+  };
+
+  const handleDecreaseWeek = () => {
+    if (currentWeek > 0) {
+      setCurrentWeek(currentWeek - 1);
+    }
+  };
   return (
     <>
       <div className={styles.header}>
@@ -20,7 +56,14 @@ const WeeklyProgress = () => {
           <div className={styles.profilePicture}></div>
           <div className={styles.profilePicture}></div>
         </div>
-        <h2 className={styles.sprintTitle}>스프린트 제목</h2>
+        <button className={styles.arrowButton} onClick={handleDecreaseWeek}>
+          &lt;
+        </button>
+        <h2 className={styles.sprintTitle}>{currentWeek}주차</h2>
+        <button className={styles.arrowButton} onClick={handleIncreaseWeek}>
+          &gt;
+        </button>
+
         <div className={styles.buttonPlaceholder}>
           <Button children="스프린트 생성" colorType="blue" size="small" onClick={handleModalOpen}></Button>
         </div>
@@ -37,18 +80,22 @@ const WeeklyProgress = () => {
 
         {/* 요일별 이슈 */}
         <div className={styles.issueGrid}>
-          {['월', '화', '수', '목', '금','날짜 미지정'].map((day) => (
+          {['월', '화', '수', '목', '금', '날짜 미지정'].map((day) => (
             <div key={day} className={styles.dayColumn}>
-              <Issue />
+              {sprintIssues?.map((issue: any) => (
+                <Issue
+                  title={issue.title}
+                  status={issue.status}
+                  epicCode={issue.epicCode}
+                  storyPoint={issue.storyPoint}
+                  issueKey={issue.issueKey}
+                />
+              ))}
             </div>
           ))}
         </div>
       </div>
-      {
-        isModalOpen && (
-          <SprintCreateModal onClose={handleModalClose}/>
-        )
-      }
+      {isModalOpen && <SprintCreateModal onClose={handleModalClose} />}
     </>
   );
 };
