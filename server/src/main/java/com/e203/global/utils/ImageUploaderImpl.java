6f90f32d6b48f8ce94e5650a961422c8724886f6
bjeeyong99 <bjeeyong99@gmail.com>
2024-11-11 01:52:48 +0900

    feat: S3 + Cloud Front 이미지 업로더 구체 클래스 구현

--- server/src/main/java/com/e203/global/utils/ImageUploaderImpl.java
+++ server/src/main/java/com/e203/global/utils/ImageUploaderImpl.java
@@ -1,13 +1,43 @@
 package com.e203.global.utils;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Component
-public class ImageUploaderImpl implements ImageUploader{
+@RequiredArgsConstructor
+public class ImageUploaderImpl implements ImageUploader {
+
+    private final AmazonS3 s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.cloudfront}")
+    private String cloudFront;
 
     @Override
     public String upload(MultipartFile profileImage) {
-        return "";
+        String fileName = profileImage.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(profileImage.getSize());
+        metadata.setContentType(profileImage.getContentType());
+
+
+        try {
+            s3Client.putObject(bucket, fileName, profileImage.getInputStream(), metadata);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = s3Client.getUrl(bucket, fileName).toString();
+
+        return cloudFront + url.split("/")[url.split("/").length - 1];
     }
 }
