bafe2e390f09ee041762f1101f78c533c232d6b5
bjeeyong99 <bjeeyong99@gmail.com>
2024-11-14 01:37:34 +0900

    feat: S3 업로드 시 파일명 UUID 적용

--- server/src/main/java/com/e203/global/utils/FileUploaderImpl.java
+++ server/src/main/java/com/e203/global/utils/FileUploaderImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -23,20 +24,23 @@ public class FileUploaderImpl implements FileUploader {
 
     @Override
     public String upload(MultipartFile profileImage) {
-        String fileName = profileImage.getOriginalFilename();
+        // 원본 파일 이름 가져오기
+        String originalFileName = profileImage.getOriginalFilename();
+        // UUID를 사용하여 고유한 파일 이름 생성
+        String fileExtension = originalFileName != null ? originalFileName.substring(originalFileName.lastIndexOf(".")) : "";
+        String uniqueFileName = UUID.randomUUID() + fileExtension;
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(profileImage.getSize());
         metadata.setContentType(profileImage.getContentType());
 
-
         try {
-            s3Client.putObject(bucket, fileName, profileImage.getInputStream(), metadata);
+            s3Client.putObject(bucket, uniqueFileName, profileImage.getInputStream(), metadata);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        String url = s3Client.getUrl(bucket, fileName).toString();
+        String url = s3Client.getUrl(bucket, uniqueFileName).toString();
 
         return cloudFront + url.split("/")[url.split("/").length - 1];
     }
