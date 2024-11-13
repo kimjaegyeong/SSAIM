96014e5c7760c148c7bdb4628759f554c7966144
강수연 <kangtea9@naver.com>
2024-11-13 17:26:54 +0900

    fix: 화자 수정 API url 수정

--- client/src/features/project/apis/meeting/editSpeakers.ts
+++ client/src/features/project/apis/meeting/editSpeakers.ts
@@ -3,7 +3,7 @@ import config from '@/config/config';
 
 export const editSpeakers = async (projectId: number, meetingId: number, requestBody: Array<{ label: string; name: string }>) => {
   try {
-    const response = await apiClient.put(`${config.BASE_URL}/projects/${projectId}/meetings/${meetingId}`, requestBody);
+    const response = await apiClient.put(`${config.BASE_URL}/projects/${projectId}/meetings/${meetingId}/speaker`, requestBody);
     console.log(response.data)
     return response.data;
   } catch (error) {
