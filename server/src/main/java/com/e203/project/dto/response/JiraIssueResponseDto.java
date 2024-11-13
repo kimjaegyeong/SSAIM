51a1464b49506965416bd49c38b7334502c5492f
김재경 <dongji_11@naver.com>
2024-11-13 09:57:32 +0900

    feat: 지라 이슈 목록 조회 시 추가로  summary, description, issuetype 반환

--- server/src/main/java/com/e203/project/dto/response/JiraIssueResponseDto.java
+++ server/src/main/java/com/e203/project/dto/response/JiraIssueResponseDto.java
@@ -9,32 +9,41 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class JiraIssueResponseDto {
-	private String title;
+	private String summary;
 	private String epicCode;
 	private String progress;
+	private String description;
 	private double storyPoint;
 	private String allocator;
 	private String issueKey;
+	private String issueType;
 
 	@Builder
-	private JiraIssueResponseDto(String issueKey, String title, String epicCode, String progress, double storyPoint, String allocator) {
+	private JiraIssueResponseDto(String issueKey, String summary, String epicCode, String progress, double storyPoint,
+		String allocator, String issueType, String description) {
 		this.issueKey = issueKey;
-		this.title = title;
+		this.summary = summary;
 		this.epicCode = epicCode;
 		this.progress = progress;
 		this.storyPoint = storyPoint;
 		this.allocator = allocator;
+		this.issueType = issueType;
+		this.description = description;
 	}
 
-	public static JiraIssueResponseDto transferDto(JiraContent jiraContent){
+	public static JiraIssueResponseDto transferDto(JiraContent jiraContent) {
 		return JiraIssueResponseDto.builder()
-			.title(jiraContent.getFields().getSummary())
+			.summary(jiraContent.getFields().getSummary())
 			.epicCode(jiraContent.getFields().getEpicCode())
 			.progress(jiraContent.getFields().getStatus().getProgress())
-			.storyPoint(jiraContent.getFields().getStoryPoint() ==null ? 0 : jiraContent.getFields().getStoryPoint())
+			.storyPoint(jiraContent.getFields().getStoryPoint() == null ? 0 : jiraContent.getFields().getStoryPoint())
 			.allocator(
-				jiraContent.getFields().getAssignee() == null ? "할당되지 않음" : jiraContent.getFields().getAssignee().getDisplayName())
+				jiraContent.getFields().getAssignee() == null ? "할당되지 않음" :
+					jiraContent.getFields().getAssignee().getDisplayName())
 			.issueKey(jiraContent.getKey())
+			.issueType(jiraContent.getFields().getIssuetype().get("name"))
+			.description(jiraContent.getFields().getDescription().getContent().get(0).getContent().get(0).get("text"))
 			.build();
+
 	}
 }
