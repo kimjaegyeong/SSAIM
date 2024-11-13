20a9fde85417331686e26f62ba7403213c9fb2cc
김재경 <dongji_11@naver.com>
2024-11-13 15:57:26 +0900

    Revert "!HOTFIX : gitlab webhook 감지를 위한 csrf 설정 추가"
    This reverts commit 03e7a26b6de3773ef4b702ea2905115747495f41

--- server/src/main/java/com/e203/config/SecurityConfig.java
+++ server/src/main/java/com/e203/config/SecurityConfig.java
@@ -65,7 +65,6 @@ public class SecurityConfig {
                     }
                 }))
                 .csrf(AbstractHttpConfigurer::disable)
-                .csrf().ignoringAntMatchers("/api/v1/notification")
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
