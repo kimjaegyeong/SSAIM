4d98f778a458ec6e29153ff9315ab553a7de2857
김재경 <dongji_11@naver.com>
2024-11-13 17:14:50 +0900

    !HOTFIX : csrf 설정 revert

--- server/src/main/java/com/e203/config/SecurityConfig.java
+++ server/src/main/java/com/e203/config/SecurityConfig.java
@@ -67,7 +67,6 @@ public class SecurityConfig {
                     }
                 }))
                 .csrf(AbstractHttpConfigurer::disable)
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/v1/notification"))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
