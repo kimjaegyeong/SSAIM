936eb77299e02ba1ec810a2cd35cd67ab01ab482
강수연 <kangtea9@naver.com>
2024-11-14 12:05:44 +0900

    feat: 우개이 상세 페이지 번호 렌더링

--- client/src/pages/remind/RemindDetailPage.tsx
+++ client/src/pages/remind/RemindDetailPage.tsx
@@ -7,16 +7,18 @@ import { DevelopStoryDTO } from '@features/remind/types/DevelopStoryDTO';
 
 interface CoverProps {
   project: DevelopStoryDTO;
+  pageIndex: number;
 }
 
 // 표지 컴포넌트
 const Cover = React.forwardRef<HTMLDivElement, CoverProps>(
-  ({ project }, ref) => (
+  ({ project, pageIndex }, ref) => (
     <div className={styles.cover} ref={ref}>
       <div className={styles.coverContent}>
         <h1 className={styles.projectName}>{project.projectName}</h1>
         <p className={styles.projectDate}>{project.projectStartDate} ~ {project.projectEndDate}</p>
       </div>
+      <div className={styles.pageNumber}>- {pageIndex + 1} -</div>
     </div>
   )
 );
@@ -24,31 +26,35 @@ const Cover = React.forwardRef<HTMLDivElement, CoverProps>(
 // 이미지 페이지 props 타입 정의
 interface ImagePageProps {
   image: string;
+  pageIndex: number;
 }
 
 // 회고 페이지 props 타입 정의
 interface ReportPageProps {
   report: string;
+  pageIndex: number;
 }
 
 // 이미지 페이지 컴포넌트
 const ImagePage = React.forwardRef<HTMLDivElement, ImagePageProps>(
-  ({ image }, ref) => (
+  ({ image, pageIndex }, ref) => (
     <div className={styles.page} ref={ref}>
       <div className={styles.imagePageContent}>
         <img src={image} alt="Weekly review" className={styles.weeklyImage} />
       </div>
+      <div className={styles.pageNumber}>- {pageIndex + 1} -</div>
     </div>
   )
 );
 
 // 회고 내용 페이지 컴포넌트
 const ReportPage = React.forwardRef<HTMLDivElement, ReportPageProps>(
-  ({ report }, ref) => (
+  ({ report, pageIndex }, ref) => (
     <div className={styles.page} ref={ref}>
       <div className={styles.reportPageContent}>
         {report}
       </div>
+      <div className={styles.pageNumber}>- {pageIndex + 1} -</div>
     </div>
   )
 );
@@ -66,6 +72,8 @@ const RemindDetailPage: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const project = location.state as DevelopStoryDTO;
+
+  let currentPageIndex = 0; 
 
   return (
     <div className={styles.mainContainer}>
@@ -102,11 +110,11 @@ const RemindDetailPage: React.FC = () => {
           showPageCorners={true}
           disableFlipByClick={false}
         >
-          <Cover project={project}/>
+          <Cover project={project} pageIndex={currentPageIndex++}/>
           {project.weeklyRemind.flatMap((data, index) => {
-            const imagePage = <ImagePage key={`image-${index}`} image={data.imageUrl} />;
-            const reportPages = splitContentToPages(data.content, 700).map((pageContent, pageIndex) => (
-              <ReportPage key={`report-${index}-${pageIndex}`} report={pageContent} />
+            const imagePage = <ImagePage key={`image-${currentPageIndex}`} image={data.imageUrl} pageIndex={currentPageIndex++}/>;
+            const reportPages = splitContentToPages(data.content, 700).map((pageContent) => (
+              <ReportPage key={`report-${index}-${currentPageIndex}`} report={pageContent} pageIndex={currentPageIndex++}/>
             ));
             return [imagePage, ...reportPages];
           })}
