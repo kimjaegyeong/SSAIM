fd50eebbf7a22ab01870f593395de536fdf2bcf1
강수연 <kangtea9@naver.com>
2024-11-15 17:31:27 +0900

    feat: 일일 회고 삭제 API 연동

--- client/src/features/project/hooks/remind/useDailyRemind.ts
+++ client/src/features/project/hooks/remind/useDailyRemind.ts
@@ -10,15 +10,19 @@ interface UseDailyRemindParams {
 }
 
 export const useDailyRemind = ({
-    projectId,
-    projectMemberId,
-    startDate,
-    endDate,
-  }: UseDailyRemindParams) => {
-    return useQuery<DailyRemindGetDTO[]>({
-      queryKey: ['dailyRemind', projectId, projectMemberId, startDate, endDate].filter(Boolean), // undefined 값 제외
-      queryFn: () => fetchDailyRemind(projectId, projectMemberId, startDate, endDate), // 항상 fetchDailyRemind 호출
-      enabled: !!projectId, // projectId가 있을 때만 쿼리 실행
-    });
-  };
+  projectId,
+  projectMemberId,
+  startDate,
+  endDate,
+}: UseDailyRemindParams) => {
+  // useQuery의 반환값 가져오기
+  const queryResult = useQuery<DailyRemindGetDTO[]>({
+    queryKey: ['dailyRemind', projectId, projectMemberId, startDate, endDate].filter(Boolean), // undefined 값 제외
+    queryFn: () => fetchDailyRemind(projectId, projectMemberId, startDate, endDate), // 항상 fetchDailyRemind 호출
+    enabled: !!projectId, // projectId가 있을 때만 쿼리 실행
+  });
+
+  // refetch 포함해서 반환
+  return { ...queryResult, refetch: queryResult.refetch };
+};
   
