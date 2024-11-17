05a24506f56391134ee1d4c55422244c7d6508ee
조성인 <sijo0209@naver.com>
2024-11-18 00:33:17 +0900

    fix: 댓글 작성 조건 수정

--- client/src/features/project/components/erd/ERD.tsx
+++ client/src/features/project/components/erd/ERD.tsx
@@ -10,7 +10,6 @@ interface ERDProps {
 const ERD: React.FC<ERDProps> = ({ projectId }) => {
   const [imageUrl, setImageUrl] = useState<string | null>(null); // 이미지 상태
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const defaultImageUrl = "https://via.placeholder.com/300"; // 기본 이미지 경로
   const [isModalOpen, setIsModalOpen] = useState(false); 
 
 
@@ -117,26 +116,44 @@ const ERD: React.FC<ERDProps> = ({ projectId }) => {
 
   return (
     <div className={styles.erd}>
-      <button onClick={handleEditClick} className={styles.editButton}>
-        편집
-      </button>
-      <input
-        type="file"
-        accept="image/*"
-        ref={fileInputRef}
-        style={{ display: "none" }}
-        onChange={handleImageUpload}
-      />
-      <img
-        src={imageUrl || defaultImageUrl}
-        alt="ERD Image"
-        className={styles.erdImage}
-        onClick={handleImageClick}
-      />
-      {isModalOpen && (
+      {imageUrl ? (
+        <>
+          <button onClick={handleEditClick} className={styles.editButton}>
+            편집
+          </button>
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={handleImageUpload}
+          />
+          <img
+            src={imageUrl}
+            alt="ERD Image"
+            className={styles.erdImage}
+            onClick={handleImageClick}
+          />
+        </>
+      ) : (
+        <>
+          <div className={styles.noImageText} onClick={handleEditClick}>
+            <p>아직 등록된 이미지가 없습니다.</p>
+            <p>여기를 눌러 ERD 이미지를 생성해주세요.</p>
+          </div>
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={handleImageUpload}
+          />
+        </>
+      )}
+      {isModalOpen && imageUrl && (
         <div className={styles.modal} onClick={handleModalClose}>
           <span className={styles.modalClose}>&times;</span>
-          <img src={imageUrl || defaultImageUrl} alt="ERD Enlarged" />
+          <img src={imageUrl} alt="ERD Enlarged" />
         </div>
       )}
     </div>
