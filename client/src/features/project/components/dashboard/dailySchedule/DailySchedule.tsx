a6d537f6d4893944316a1d0924de472fc70a93ed
여대기 <daegi0923@naver.com>
2024-11-17 16:21:26 +0900

    feat : 대시보드 외부링크 연결 및 요일 날짜 디자인 변경

--- client/src/features/project/components/dashboard/dailySchedule/DailySchedule.tsx
+++ client/src/features/project/components/dashboard/dailySchedule/DailySchedule.tsx
@@ -14,10 +14,24 @@ interface DailyScheduleProps {
 }
 
 const DailySchedule: React.FC<DailyScheduleProps> = ({ day, date, jiraData, gitlabData, meetingData }) => {
+  const dayToKr = (day: DayOfWeek) => {
+    const dayMap: Record<DayOfWeek, string> = {
+      Sunday: '일요일',
+      Monday: '월요일',
+      Tuesday: '화요일',
+      Wednesday: '수요일',
+      Thursday: '목요일',
+      Friday: '금요일',
+      Saturday: '토요일',
+    };
+    return dayMap[day];
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        {day}, {date}
+        <span>{date}</span>
+        <span>{dayToKr(day)}</span>
       </div>
       <div className={styles.content}>
         {jiraData?.length > 0 && <Task taskType="jira" day={day} tasks={jiraData} />}
